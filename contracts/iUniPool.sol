a43ebdfaa5d0277c86500fb69732c352229486d8
amateur-dev <dipesh.sukhani@gmail.com>
2020-02-24 17:07:20 +0800

    Updated till the getPrice

--- contracts/iUniPool.sol
+++ contracts/iUniPool.sol
@@ -17,6 +17,9 @@ contract iUniPool {
         0xC011A72400E58ecD99Ee497CF89E3775d4bd732F
     );
 
+    mapping(address => uint256) LPTokensSupplied;
+    uint256 public totalLPTokensStaked;
+
     constructor() public {
         approve_UniswapLTAddress();
     }
@@ -36,12 +39,80 @@ contract iUniPool {
     //     return Unipool(UnipoolAddress).balanceOf(address(this));
     // }
 
-    function getReward(address _ofWhose) public view returns (uint256) {
-        return Unipool(UnipoolAddress).earned(address(_ofWhose));
+    function getRewardOUT() internal returns (uint256) {
+        Unipool(UnipoolAddress).getReward();
+    }
+
+    function howMuchHaveIEarned() public view returns (uint256) {
+        return Unipool(UnipoolAddress).earned(address(this));
     }
 
     // function getReward() public view returns (uint256) {
     //     return Unipool(UnipoolAddress).earned(address(this));
     // }
+
+    function getMyShareOfLPTokens(address _ofWhose)
+        public
+        view
+        returns (uint256)
+    {
+        return LPTokensSupplied[_ofWhose];
+    }
+
+    function stakeMyShare(uint256 _LPTokenUnits) public {
+        // basic check
+        require(
+            (_LPTokenUnits >= 1000000000),
+            "less than the minimum required"
+        );
+        require(
+            (UniswapLiquityTokenAddress.allowance(msg.sender, address(this)) >=
+                _LPTokenUnits),
+            "Allowance Granted to this contract is less"
+        );
+        require(
+            (UniswapLiquityTokenAddress.balanceOf(msg.sender) >= _LPTokenUnits),
+            "user balance is less than tokens requested to be staked"
+        );
+
+        // finding out the price
+        uint256 SNXPerLPToken = getPrice();
+
+        // transfer to this address
+        require(
+            (
+                UniswapLiquityTokenAddress.transferFrom(
+                    msg.sender,
+                    address(this),
+                    _LPTokenUnits
+                )
+            ),
+            "issue in transferring tokens"
+        );
+        LPTokensSupplied[msg.sender] += _LPTokenUnits;
+
+        // staking the LP Tokens
+        uint256 newtotalLPTokensStaked = totalLPTokensStaked;
+        newtotalLPTokensStaked += _LPTokenUnits;
+        Unipool(UnipoolAddress).stake(_LPTokenUnits);
+        require(
+            Unipool(UnipoolAddress).balanceOf(address(this)) ==
+                newtotalLPTokensStaked
+        );
+        totalLPTokensStaked = newtotalLPTokensStaked;
+
+    }
+
+    function getPrice() public returns (uint256) {
+        uint256 currentSNXHoldings = SNXTokenAddress.balanceOf(address(this));
+        uint256 SNXEarned = howMuchHaveIEarned();
+        uint256 LPTokensHoldings = totalLPTokensStaked;
+        require(
+            totalLPTokensStaked ==
+                (Unipool(UnipoolAddress).balanceOf(address(this))),
+            "error1"
+        );
+        // updated till here! Working from here.
+    }
 
 }
